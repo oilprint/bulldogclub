48b97a1a56f2dfe63c82bbe30a854e26a1d40a92
oilprint <irynakozlova.k@gmail.com>
2023-06-08 15:53:59 +0300

    started slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,5 @@
 $(function () {
-  var mixer = mixitup('.products__grid');
+  
 
   $('.review__inner').slick({
     dots: true,
@@ -7,7 +7,27 @@ $(function () {
     fade: true,
     prevArrow: '<button type="button" class="slick-arrow slick-arrow--prev"><span class="sr-only">Предыдущий слайд</span><svg class="slick-arrow__icon" width="32" height="32"><use xlink:href="images/symbol-defs.svg#icon-chevron"></use></svg></button>',
     nextArrow: '<button type="button" class="slick-arrow slick-arrow--next"><span class="sr-only">Следующий слайд</span><svg class="slick-arrow__icon" width="32" height="32"><use xlink:href="images/symbol-defs.svg#icon-chevron"></use></svg></button>'
+  });
 
+  const slider = document.querySelector('.swiper');
+
+  const swiper = new Swiper(slider, {
+    slidesPerView: 3,
+    spaceBetween: 30, 
+    
+
+    // autoHeight:	false,
+  
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true,
+    },
+  
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
   });
   
 });
