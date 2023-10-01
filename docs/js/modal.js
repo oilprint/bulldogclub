e4967f5421f9d61569d638a05a299b841afd2322
oilprint <irynakozlova.k@gmail.com>
2023-10-01 13:42:50 +0300

    change body.lock

--- docs/js/modal.js
+++ docs/js/modal.js
@@ -4,20 +4,25 @@ $(function () {
   const buttons = document.querySelectorAll('[data-modal-button]');
   const modal = document.querySelector('[data-modal]');
   const buttonClose = document.querySelector('[data-modal-close]');
+  const bodyLock = document.querySelector('body'); 
  
 
   buttons.forEach(function (item) {
     item.addEventListener('click', function () {
       modal.classList.remove('hidden');
+      bodyLock.classList.add('lock');
     });
   });
  
   buttonClose.addEventListener('click', function () {
     modal.classList.add('hidden');
+    bodyLock.classList.remove('lock');
+    
   });
 
   modal.addEventListener('click', function ( ) {
     modal.classList.add('hidden');   
+    bodyLock.classList.remove('lock');
   });
 
 
